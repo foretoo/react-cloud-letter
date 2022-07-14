7a296604b2ad8746f9e54f04f13cf76b9afd25fc
Sergey Borovikov <serg.there@gmail.com>
2022-07-15 00:16:43 +0300

    init vite env

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.sass"
+
+const root = ReactDOM.createRoot(document.getElementById("root")!)
+
+root.render(
+  <React.StrictMode>
+    <h1>cloud-letter</h1>
+  </React.StrictMode>,
+)

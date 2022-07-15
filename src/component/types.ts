c436642cb0dede1da95bb628116f9d4fa39fe717
Sergey Borovikov <serg.there@gmail.com>
2022-07-15 19:08:51 +0300

    add CSSFontProperties

--- src/component/types.ts
+++ src/component/types.ts
@@ -2,10 +2,22 @@ type CloudWordProps = {
   children: string
 }
 
+type CSSFontProperties = Partial<{
+  font: string
+  fontFamily: string
+  fontSize: string
+  fontStyle: string
+  fontVariant: string
+  fontWeight: string
+  fontStretch: string
+  lineHeight: string
+}>
+
 type CloudLetterProps = {
   children: JSX.Element | JSX.Element[] | string | null,
   width: string,
-  spaceWidth: string
+  spaceWidth: string,
+  fontStyle: CSSFontProperties
 }
 
 export {

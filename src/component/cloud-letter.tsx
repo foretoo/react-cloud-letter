ca3c2558de2f7ddd2a9d12378653a267c9711b57
Sergey Borovikov <serg.there@gmail.com>
2022-07-15 23:59:10 +0300

    storing data from cloud refs

--- src/component/cloud-letter.tsx
+++ src/component/cloud-letter.tsx
@@ -1,5 +1,5 @@
-import { Fragment, CSSProperties, useRef } from "react"
-import { CloudLetterProps } from "./types"
+import { Fragment, CSSProperties, useState, useLayoutEffect, useRef } from "react"
+import { CloudData, CloudLetterProps } from "./types"
 import { CloudWord } from "./cloud-word"
 import { CloudSpace } from "./cloud-space"
 import "./cloud.sass"
@@ -10,10 +10,24 @@ const CloudLetter = ({
   children: content,
   width,
   spaceWidth,
-  fontStyle
+  fontStyle,
 }: CloudLetterProps) => {
 
+  const [ data, setData ] = useState<CloudData[]>([])
   const cloudsRef = useRef<HTMLSpanElement[]>([])
+
+  useLayoutEffect(() => {
+    setData(
+      cloudsRef.current.map(({
+        offsetLeft, offsetTop, offsetWidth, offsetHeight
+      }) => ({
+        x: offsetLeft,
+        y: offsetTop,
+        w: offsetWidth,
+        h: offsetHeight
+      }))
+    )
+  }, [ content, width, spaceWidth, fontStyle ])
 
   if (typeof content === "string") {
     const words = content.match(/[^\s]+/g)

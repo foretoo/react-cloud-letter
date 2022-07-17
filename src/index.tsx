dfc8393f0ef4bf97c68685c4c4a478809d00bb8e
Sergey Borovikov <serg.there@gmail.com>
2022-07-17 19:09:15 +0300

    ~

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root")!)
 const width = window.innerWidth
 
 root.render(
-  <CloudLetter width={`${width}px`} spaceWidth="20px">
+  <CloudLetter width={`${width/2|0}px`} spaceWidth="40px">
     {text}
     {/* Initial  <CloudWord>Text</CloudWord> <CloudWord>For</CloudWord> <CloudWord>Cloudy</CloudWord> Lettering */}
   </CloudLetter>

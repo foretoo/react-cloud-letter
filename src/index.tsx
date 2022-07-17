b7f76d343318ac314feca2f6b10d0ab45ba1bc48
Sergey Borovikov <serg.there@gmail.com>
2022-07-17 18:04:38 +0300

    big paragraph check

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from "react-dom/client"
 import { CloudLetter, CloudWord } from "./component/cloud-letter"
+import { text } from "./component/text"
 import "./index.sass"
 
 const root = ReactDOM.createRoot(document.getElementById("root")!)
 
+const width = window.innerWidth
+
 root.render(
-  <CloudLetter width="200px" spaceWidth="20px">
-    Initial  <CloudWord>Text</CloudWord> <CloudWord>For</CloudWord> <CloudWord>Cloudy</CloudWord> Lettering
+  <CloudLetter width={`${width}px`} spaceWidth="20px">
+    {text}
+    {/* Initial  <CloudWord>Text</CloudWord> <CloudWord>For</CloudWord> <CloudWord>Cloudy</CloudWord> Lettering */}
   </CloudLetter>
 )

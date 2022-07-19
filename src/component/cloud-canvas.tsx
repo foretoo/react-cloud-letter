fe251473bfad3a25a8d3e4a75d0ac6b91b778005
Sergey Borovikov <serg.there@gmail.com>
2022-07-19 16:01:13 +0300

    add debugging of align to center

--- src/component/cloud-canvas.tsx
+++ src/component/cloud-canvas.tsx
@@ -78,24 +78,33 @@ export const CloudCanvas = (
     let debug = true
     if (debug) {
 
+      const findMin = () => (
+        cloudRects.reduce((min, curr) => (
+          curr[0][0][0] < min ? curr[0][0][0] : min
+        ), width)
+      )
+      const min = findMin()
+
       ctx.strokeStyle = "#f74"
       ctx.lineWidth = 1
 
       const h = cloudRects[0][0][2][1] - cloudRects[0][0][0][1]
-      const hh = h / 2
+      const hh = align === "center" ? h : h / 2
 
       const proceedX = (
         fn: (x: number, y: number) => void,
         y: number
       ) => {
         if (align === "left")
-          for (let x = 0; x < width / hh; x++) fn(x, y)
+          for (let x = 0; x < width; x+=hh) fn(x, y)
         else if (align === "right")
-          for (let x = width / hh; x > 0; x--) fn(x, y)
+          for (let x = width; x > 0; x-=hh) fn(x, y)
+        else if (align === "center")
+          for (let x = min; x < width; x+=hh) fn(x, y)
       }
       const drawLine = (x: number, y: number) => {
-        ctx.moveTo(x * hh * pr, y * h * pr)
-        ctx.lineTo(x * hh * pr, (y + 1) * h * pr)
+        ctx.moveTo(x * pr, y * h * pr)
+        ctx.lineTo(x * pr, (y + 1) * h * pr)
       }
 
       ctx.beginPath()

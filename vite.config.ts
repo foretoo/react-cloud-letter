8008ee7fb4ed49b2023b4cc1b0049db3dfbd1347
Sergey Borovikov <serg.there@gmail.com>
2022-07-21 22:38:24 +0300

    separate dev/build index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,24 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react() ],
+  build: {
+    // minify: false,
+    assetsDir: ".",
+    rollupOptions: {
+      input: "/src/index.tsx",
+      // output: {
+      //   dir: "dist",
+      //   assetFileNames: "style.css",
+      //   entryFileNames: "bundle.js",
+      // },
+      external: [
+        "react",
+        "react-dom"
+      ],
+    }
+  },
   server: {
-    open: true,
+    open: "/src/index.html",
     host: true,
   },
 })

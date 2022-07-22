0ed986e88b63e21933bec3b46f7850177ea83c35
Sergey Borovikov <serg.there@gmail.com>
2022-07-23 02:35:19 +0300

    add font, padding, fill, stroke, shadow states

--- src/app.tsx
+++ src/app.tsx
@@ -6,51 +6,47 @@ const width = window.innerWidth
 const mineText = "The ${origin} of the term ${\"cloud\"} can be ${found} in ${the Old English} words clud or clod, meaning a hill or a mass of stone."
 // "The $origin$ of the term \"cloud\" can be $found$ in $the Old English$ words clud or clod, meaning a hill or a mass of stone."
 
+type Mode = "WORD" | "SPACE" | "PARTIAL"
+type Align = "left" | "center" | "right"
+
 export const App = () => {
 
   const [ content, setContent ] = useState(mineText)
-  const [ mode, setMode ] = useState<"WORD" | "SPACE" | "PARTIAL">("PARTIAL")
-  const [ align, setAlign ] = useState<"left" | "center" | "right">("center")
-  const [ spaceWidth, setSpaceWidth ] = useState(4)
-  const [ cloudHeight, setCloudHeight ] = useState(32)
-  const [ snap, setSnap ] = useState(0)
-  const [ grid, setGrid ] = useState(true)
+  const [ mode, setMode ] = useState<Mode>("PARTIAL")
+  const [ align, setAlign ] = useState<Align>("center")
+  const [ spaceWidth, setSpaceWidth ] = useState(0)
+  const [ cloudHeight, setCloudHeight ] = useState(33)
+  const [ padding, setPadding ] = useState(16)
+  const [ snap, setSnap ] = useState(1)
+  const [ grid, setGrid ] = useState(false)
+  const [ fill, setFill ] = useState("#ffffff")
+  const [ stroke, setStroke ] = useState("#1A66A1")
+  const [ shadowOffset, setShadowOffset ] = useState({ x: -20, y: 20 })
 
-  const handleInput = (e: FormEvent<HTMLTextAreaElement>) => {
-    const target = e.target as HTMLTextAreaElement
-    setContent(target.value)
-  }
-  const handleModeChange = (e: FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement
-    setMode(target.value as "WORD" | "SPACE")
-  }
-  const handleAlignChange = (e: FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement
-    setAlign(target.value as "left" | "center" | "right")
-  }
-  const handleSpaceWidthChange = (e: FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement
-    setSpaceWidth(+target.value)
-  }
-  const handleCloudHeightChange = (e: FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement
-    setCloudHeight(+target.value)
-  }
-  const handleSnapChange = (e: FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement
-    setSnap(+target.value)
-  }
+    
 
   return (
     <>
       <CloudLetter
-        width={Math.min(width*0.8, 512)}
+        width={Math.min(width * 0.8, 512)}
         spaceWidth={spaceWidth}
         cloudHeight={cloudHeight}
+        font={{
+          color: stroke,
+          family: "Poppins",
+          size: 16
+        }}
+        padding={padding}
+
         align={align}
         mode={mode}
         snap={snap}
         grid={grid}
+
+        fill={fill}
+        stroke={stroke}
+        shadowOffsetX={shadowOffset.x}
+        shadowOffsetY={shadowOffset.y}
       >
         {content}
         {/* Initial  <CloudWord>Text</CloudWord> <CloudWord>For</CloudWord> <CloudWord>Cloudy</CloudWord> Lettering */}
@@ -61,10 +57,55 @@ export const App = () => {
           <span>content:</span>
           <textarea
             rows={3}
-            cols={Math.min(Math.max(42, width/25|0), 55)}
+            cols={Math.min(Math.max(42, width / 25 | 0), 55)}
             value={content}
-            onInput={handleInput}
+            onInput={(e) => setContent((e.target as HTMLTextAreaElement).value)}
           />
+        </fieldset>
+
+        <fieldset className="controls-row view">
+          <span>view:</span>
+          <label>
+            fill
+            <div className="hidder">
+              <input
+                type="color"
+                value={fill}
+                onChange={(e) => setFill((e.target as HTMLInputElement).value)}
+              />
+            </div>
+          </label>
+          <label>
+            stroke
+            <div className="hidder">
+              <input
+                type="color"
+                value={stroke}
+                onChange={(e) => setStroke((e.target as HTMLInputElement).value)}
+              />
+            </div>
+          </label>
+          <span className="shadow-label">shadow:</span>
+          <label>
+            x
+            <input
+              type="number"
+              value={shadowOffset.x}
+              min={-20}
+              max={20}
+              onChange={(e) => setShadowOffset((prev) => ({ x: +e.target.value, y: prev.y }))}
+            />
+          </label>
+          <label>
+            y
+            <input
+              type="number"
+              value={shadowOffset.y}
+              min={-20}
+              max={20}
+              onChange={(e) => setShadowOffset((prev) => ({ y: +e.target.value, x: prev.x }))}
+            />
+          </label>
         </fieldset>
 
         <fieldset className="controls-row mode">
@@ -75,7 +116,7 @@ export const App = () => {
               name="mode"
               value="WORD"
               checked={mode === "WORD"}
-              onChange={handleModeChange}
+              onChange={(e) => setMode((e.target as HTMLInputElement).value as Mode)}
             />
             WORD
           </label>
@@ -85,7 +126,7 @@ export const App = () => {
               name="mode"
               value="PARTIAL"
               checked={mode === "PARTIAL"}
-              onChange={handleModeChange}
+              onChange={(e) => setMode((e.target as HTMLInputElement).value as Mode)}
             />
             PARTIAL
           </label>
@@ -95,7 +136,7 @@ export const App = () => {
               name="mode"
               value="SPACE"
               checked={mode === "SPACE"}
-              onChange={handleModeChange}
+              onChange={(e) => setMode((e.target as HTMLInputElement).value as Mode)}
             />
             SPACE
           </label>
@@ -109,7 +150,7 @@ export const App = () => {
               name="align"
               value="left"
               checked={align === "left"}
-              onChange={handleAlignChange}
+              onChange={(e) => setAlign((e.target as HTMLInputElement).value as Align)}
             />
             left
           </label>
@@ -119,7 +160,7 @@ export const App = () => {
               name="align"
               value="center"
               checked={align === "center"}
-              onChange={handleAlignChange}
+              onChange={(e) => setAlign((e.target as HTMLInputElement).value as Align)}
             />
             center
           </label>
@@ -129,44 +170,59 @@ export const App = () => {
               name="align"
               value="right"
               checked={align === "right"}
-              onChange={handleAlignChange}
+              onChange={(e) => setAlign((e.target as HTMLInputElement).value as Align)}
             />
             right
           </label>
         </fieldset>
 
-        <fieldset className="controls-row space-width">
-          <span>space width</span>
-          <input
-            type="range"
-            min={0}
-            max={100}
-            step={1}
-            value={spaceWidth}
-            onInput={handleSpaceWidthChange}
-          />
-        </fieldset>
-
-        <fieldset className="controls-row cloud-height">
-          <span>clouds height</span>
-          <input
-            type="range"
-            min={20}
-            max={50}
-            step={1}
-            value={cloudHeight}
-            onInput={handleCloudHeightChange}
-          />
+        <fieldset className="controls-row clouds">
+          <span>cloud's:</span>
+          <div>
+            <label>
+              height
+              <input
+                type="range"
+                min={20}
+                max={50}
+                step={1}
+                value={cloudHeight}
+                onInput={(e) => setCloudHeight(+(e.target as HTMLInputElement).value)}
+              />
+            </label>
+            <label>
+              width
+              <input
+                type="range"
+                min={0}
+                max={50}
+                step={1}
+                value={padding}
+                onInput={(e) => setPadding(+(e.target as HTMLInputElement).value)}
+              />
+            </label>
+            <label>
+              space between
+              <input
+                type="range"
+                min={0}
+                max={100}
+                step={1}
+                value={spaceWidth}
+                onInput={(e) => setSpaceWidth(+(e.target as HTMLInputElement).value)}
+              />
+            </label>
+          </div>
         </fieldset>
 
         <fieldset className="controls-row snap">
           <span>snap: </span>
           <label className="snap">
-            cloudHeight  / 
+            cloudHeight  /
             <input
               type="number"
               value={snap}
-              onChange={handleSnapChange}
+              onChange={(e) => setSnap(+(e.target as HTMLInputElement).value)}
             />
           </label>
           <label className="grid">
@@ -175,7 +231,7 @@ export const App = () => {
               type="checkbox"
               disabled={!snap}
               checked={grid}
-              onChange={() => setGrid(prev => !prev)}
+              onChange={() => setGrid((prev) => !prev)}
             />
           </label>
         </fieldset>

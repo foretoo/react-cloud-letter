d5a4c6a2c83d283fde844ab2868ec0b9681f2193
Sergey Borovikov <serg.there@gmail.com>
2022-07-23 18:57:03 +0300

    add public builder config

---
+++ config/public.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  mode: "production",
+  publicDir: false,
+  assetsInclude: ".css",
+  plugins: [ react() ],
+  build: {
+    minify: true,
+    emptyOutDir: false,
+    assetsDir: ".",
+    rollupOptions: {
+      input: "/src/index.tsx",
+      output: {
+        dir: "public",
+        assetFileNames: "style.css",
+        entryFileNames: "bundle.js",
+      },
+      external: [
+        "react",
+        "react-dom"
+      ],
+    }
+  },
+})

fcccd20e7adfabb3517b1f5b5216684feb05a2f8
Sergey Borovikov <serg.there@gmail.com>
2022-07-23 15:42:15 +0300

    separate build config

---
+++ config/build.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [ react() ],
+  build: {
+    // minify: false,
+    assetsDir: ".",
+    rollupOptions: {
+      input: "/src/index.tsx",
+      // output: {
+      //   dir: "dist",
+      //   assetFileNames: "style.css",
+      //   entryFileNames: "bundle.js",
+      // },
+      external: [
+        "react",
+        "react-dom"
+      ],
+    }
+  },
+})

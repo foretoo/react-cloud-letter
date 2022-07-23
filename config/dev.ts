c6a9573403af661523addb4d8758a1ef6006ef2d
Sergey Borovikov <serg.there@gmail.com>
2022-07-23 17:45:55 +0300

    add development mode

--- config/dev.ts
+++ config/dev.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  mode: "development",
   plugins: [ react() ],
   server: {
     open: "/src/index.html",
